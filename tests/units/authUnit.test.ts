055c110f9d28563a511acb59d2da52598f35bd76
Ivan <ivanaires32@gmail.com>
2023-12-01 12:16:18 -0300

    test: auth unit test

--- tests/units/authUnit.test.ts
+++ tests/units/authUnit.test.ts
@@ -1,17 +1,14 @@
 import authRepository from "../../src/repository/auth.repository";
 import authService from "../../src/service/auth.service"
-import userFaker from "../factorys/user.factory";
 import signUpFaker from "../factorys/signUp.factory";
-import { ObjectId } from "mongodb";
 
 describe('auth unit tests', () => {
     it("should return 409 when email already register", async () => {
-        const email = "ivan"
         jest.spyOn(authRepository, "findByEmail").mockImplementationOnce((): any => {
             return true
         });
     
-        const user = signUpFaker(email)
+        const user = signUpFaker()
         const response = authService.create(user)
         expect(response).rejects.toEqual(
             { message: "Email already use", name: "conflict" }
